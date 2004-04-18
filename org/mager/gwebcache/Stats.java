2e57555799084797cb1c1611cfe81c8cc8bed72f
Jens-Uwe Mager <jum@anubis.han.de>
2004-04-18 21:52:31 +0000

    Fix some indentation issues.

--- org/mager/gwebcache/Stats.java
+++ org/mager/gwebcache/Stats.java
@@ -26,14 +26,14 @@ public class Stats  implements Serializable {
      * The hour we last updated the hourly statistics.
      */
     public volatile long lastHour;
-	/**
-	 * The day we last updated the dayly statistics.
-	 */
-	public volatile long lastDay;
-	/**
-	 * The week we last updated the weekly statistics.
-	 */
-	public volatile long lastWeek;
+    /**
+     * The day we last updated the daily statistics.
+     */
+    public volatile long lastDay;
+    /**
+     * The week we last updated the weekly statistics.
+     */
+    public volatile long lastWeek;
     /**
      * The number of requests received.
      */
@@ -44,61 +44,61 @@ public class Stats  implements Serializable {
      */
     public volatile Counter numUpdates;
 
-	/**
-	 * The number of urlfile requests received.
-	 * This is a GWB1 requests
-	 */
-	public volatile Counter urlRequests;
+    /**
+     * The number of urlfile requests received.
+     * This is a GWB1 requests
+     */
+    public volatile Counter urlRequests;
+    
+    /**
+     * The number of hostfile requests received.
+     * This is a GWB1 requests
+     */
+    public volatile Counter hostRequests;
+    
+    /**
+     * The number of get requests received.
+     * This is a GWB2 requests for url and host
+     */
+    public volatile Counter getRequests;
 	
-	/**
-	 * The number of hostfile requests received.
-	 * This is a GWB1 requests
-	 */
-	public volatile Counter hostRequests;
-	
-	/**
-	 * The number of get requests received.
-	 * This is a GWB2 requests for url and host
-	 */
-	public volatile Counter getRequests;
-	
-	/**
-	 * The number of request by client 
-	 * We don't differenciate between version
-	 * String(client) -> Counter
-	 */
-	public Map clientRequests;
+    /**
+     * The number of request by client 
+     * We don't differenciate between version
+     * String(client) -> Counter
+     */
+    public Map clientRequests;
 
-	/**
-	 * The number of request by client/version 
-	 * We differenciate between version
-	 * ClientVersion -> Counter
-	 */
-	public Map clientVersionRequests;
-	
-	/**
-	 * The only instance of the stats data.
-	 */
-	private static Stats instance = new Stats();
-	private Stats() {
-		clientVersionRequests =
-				Collections.synchronizedMap( new HashMap());
-		clientRequests =
-				Collections.synchronizedMap( new HashMap());
-		getRequests = new Counter();
-		hostRequests = new Counter();
-		urlRequests = new Counter();
-		numUpdates = new Counter();
-		numRequests = new Counter();
-		startTime = new Date();
-	}
-	/**
-	* Retrieve the current cache Stats instance.
-	* @return A Stats obejct.
-	*/
-   public static Stats getInstance() {
-	   return instance;
-   }
+    /**
+     * The number of request by client/version 
+     * We differenciate between version
+     * ClientVersion -> Counter
+     */
+    public Map clientVersionRequests;
+    
+    /**
+     * The only instance of the stats data.
+     */
+    private static Stats instance = new Stats();
+    private Stats() {
+        clientVersionRequests =
+                        Collections.synchronizedMap( new HashMap());
+        clientRequests =
+                        Collections.synchronizedMap( new HashMap());
+        getRequests = new Counter();
+        hostRequests = new Counter();
+        urlRequests = new Counter();
+        numUpdates = new Counter();
+        numRequests = new Counter();
+        startTime = new Date();
+    }
+    /**
+     * Retrieve the current cache Stats instance.
+     * @return A Stats obejct.
+     */
+    public static Stats getInstance() {
+        return instance;
+    }
     /**
      * Check if we are past the current hour and move the
      * data from this hour to that of the previous hour.
@@ -109,23 +109,23 @@ public class Stats  implements Serializable {
         long thisHour = now.getTime() / Data.MILLIS_PER_HOUR;
         long thisDay = now.getTime() / Data.MILLIS_PER_DAY;
         long thisWeek = now.getTime()/ Data.MILLLIS_PER_WEEK;
-		if (thisDay != lastDay) {
-			lastDay = thisDay;
-			
-			numRequests.bumpDay();
-			numUpdates.bumpDay();
-			getRequests.bumpDay();
-			urlRequests.bumpDay();
-			hostRequests.bumpDay();
-			//Requests by client
-			for(Iterator i = clientRequests.entrySet().iterator();i.hasNext();){
-				((Counter)((Map.Entry)i.next()).getValue()).bumpDay();
-			}
-			//Request by client/version
-			for(Iterator i = clientVersionRequests.entrySet().iterator();i.hasNext();){
-				((Counter)((Map.Entry)i.next()).getValue()).bumpDay();
-			}
-		} else if (thisHour != lastHour) {
+        if (thisDay != lastDay) {
+            lastDay = thisDay;
+
+            numRequests.bumpDay();
+            numUpdates.bumpDay();
+            getRequests.bumpDay();
+            urlRequests.bumpDay();
+            hostRequests.bumpDay();
+            //Requests by client
+            for(Iterator i = clientRequests.entrySet().iterator();i.hasNext();){
+                ((Counter)((Map.Entry)i.next()).getValue()).bumpDay();
+            }
+            //Request by client/version
+            for(Iterator i = clientVersionRequests.entrySet().iterator();i.hasNext();){
+                ((Counter)((Map.Entry)i.next()).getValue()).bumpDay();
+            }
+        } else if (thisHour != lastHour) {
             lastHour = thisHour;
             numRequests.bumpHour();
             numUpdates.bumpHour();
@@ -133,9 +133,9 @@ public class Stats  implements Serializable {
             urlRequests.bumpHour();
             hostRequests.bumpHour();
             //Requests by client
-			for(Iterator i = clientRequests.entrySet().iterator();i.hasNext();){
-				((Counter)((Map.Entry)i.next()).getValue()).bumpHour();
-			}
+            for(Iterator i = clientRequests.entrySet().iterator();i.hasNext();){
+                ((Counter)((Map.Entry)i.next()).getValue()).bumpHour();
+            }
             //Request by client/version
             for(Iterator i = clientVersionRequests.entrySet().iterator();i.hasNext();){
             	((Counter)((Map.Entry)i.next()).getValue()).bumpHour();
@@ -144,67 +144,67 @@ public class Stats  implements Serializable {
 
     }
 
-	/**
-	 * Increment the number of requests received.
-	 */
+    /**
+     * Increment the number of requests received.
+     */
     public void bumpRequests() {
         numRequests.bumpCount();
     }
 
-	/**
-	 * Increment the number of updates received.
-	 */
+    /**
+     * Increment the number of updates received.
+     */
     public void bumpUpdates() {
         numUpdates.bumpCount();
     }
     
-	/**
-	 * Increment the number of urlfile request
-	 * This is a GWC1 request
-	 */
-	public void bumpUrlRequests(){
-		urlRequests.bumpCount();
-	}
+    /**
+     * Increment the number of urlfile request
+     * This is a GWC1 request
+     */
+    public void bumpUrlRequests(){
+        urlRequests.bumpCount();
+    }
 	
-	/**
-	 * Increment the number of host request
-	 * This is a GWC1 request
-	 */
-	public void bumpHostRequests(){
-		hostRequests.bumpCount();
-	}
+    /**
+     * Increment the number of host request
+     * This is a GWC1 request
+     */
+    public void bumpHostRequests(){
+        hostRequests.bumpCount();
+    }
 	
-	/**
-	 * Increment the number of get request
-	 * This is a GWC2 request
-	 */
-	public void bumpGetRequests(){
-		getRequests.bumpCount();
-	}
+    /**
+     * Increment the number of get request
+     * This is a GWC2 request
+     */
+    public void bumpGetRequests(){
+        getRequests.bumpCount();
+    }
 	
-	/**
-	 * This bump the client only and client/version count
-	 * @param c
-	 */
-	public void bumpByClient(ClientVersion c) {
-		String client = c.getClient();
+    /**
+     * This bump the client only and client/version count
+     * @param c
+     */
+    public void bumpByClient(ClientVersion c) {
+        String client = c.getClient();
 
-		//This is not very well syncronized, but a mistake
-		//make that we will not count 1 request
+        //This is not very well syncronized, but a mistake
+        //make that we will not count 1 request
 		
-		//We bump the number of request for this client
-		Counter count = (Counter)clientRequests.get(client);
-		if(count == null){
-			clientRequests.put(client,new Counter(1));
-		} else
-			count.bumpCount();
-		
-		//We bump the number of request for this client/version
-		count = (Counter)clientVersionRequests.get(c);
-		if(count == null){
-			clientVersionRequests.put(c,new Counter(1));
-		} else
-			count.bumpCount();
-	} 
+        //We bump the number of request for this client
+        Counter count = (Counter)clientRequests.get(client);
+        if(count == null){
+            clientRequests.put(client,new Counter(1));
+        } else
+            count.bumpCount();
+
+        //We bump the number of request for this client/version
+        count = (Counter)clientVersionRequests.get(c);
+        if(count == null){
+            clientVersionRequests.put(c,new Counter(1));
+        } else
+            count.bumpCount();
+    } 
 
 }
